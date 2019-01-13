7d8436ea3d700214dff891481b339cc7e49700eb
Claus Bo Hansen <sbo@trolling.dk>
2019-01-13 18:50:26 +0100

    End Sunday evening

--- getRVersion.java
+++ getRVersion.java
@@ -9,6 +9,8 @@ public class getRVersion {
             REXPMismatchException {
         RConnection c = new RConnection();
         REXP x = c.eval("R.version.string");
+//        REXP x = c.eval("tt()");
+//        REXP x = c.eval("1+1");
         System.out.println(x.asString());
     }
 }
